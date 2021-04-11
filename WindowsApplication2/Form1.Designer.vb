bb9bafc01d33c3b99d3282798881d5389f4594ed
Akile ODAY <Akile ODAY@DESKTOP-VC6BBKV>
2021-04-11 23:02:10 +0300

    Add project files.

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ekleButton = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ekleButton)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(36, 51)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(456, 130)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(72, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Isim"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(72, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Yaş"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(167, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(167, 68)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'ekleButton
+        '
+        Me.ekleButton.Location = New System.Drawing.Point(352, 91)
+        Me.ekleButton.Name = "ekleButton"
+        Me.ekleButton.Size = New System.Drawing.Size(75, 23)
+        Me.ekleButton.TabIndex = 4
+        Me.ekleButton.Text = "Ekle"
+        Me.ekleButton.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(36, 205)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(579, 367)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ekleButton As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+	Dim cnn As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Akile ODAY\OneDrive - Eastern Mediterranean University\Desktop\bahar 2019-2020\BTEP210\ders notlari\5-veritabani bilgi girisi\WindowsApplication2(ozluk)\WindowsApplication2\Database1.mdf"";Integrated Security=True")
+	Dim cmd As New SqlCommand
+    Dim ds As New DataSet
+    Dim da As New SqlDataAdapter
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If cnn.State = ConnectionState.Closed Then
+            cnn.Open()
+            cmd.Connection = cnn
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select isim, yas from [ozluk]"
+            da.SelectCommand = cmd
+            da.Fill(ds)
+            DataGridView1.DataSource = ds.Tables(0)
+            cnn.Close()
+        End If
+    End Sub
+
+    Private Sub ekleButton_Click(sender As Object, e As EventArgs) Handles ekleButton.Click
+        If cnn.State = ConnectionState.Closed Then
+            cnn.Open()
+            ds.Clear()
+            cmd.Connection = cnn
+            cmd.CommandType = CommandType.Text
+            cmd.Parameters.Clear()
+            cmd.CommandText = "insert into [ozluk](isim,yas) values(@isim,@yas)"
+            cmd.Parameters.Add("@isim", SqlDbType.NVarChar).Value = TextBox1.Text
+            cmd.Parameters.Add("@yas", SqlDbType.NChar).Value = TextBox2.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("kayit eklendi")
+            TextBox1.Clear()
+            TextBox2.Clear()
+            cmd.CommandText = "select isim,yas from [ozluk]"
+            da.SelectCommand = cmd
+            da.Fill(ds)
+            DataGridView1.DataSource = ds.Tables(0)
+            cnn.Close()
+        End If
+    End Sub
+End Class
