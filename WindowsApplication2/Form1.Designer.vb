d54bf2bbb44f4c5ed921e2771994fd8f50937d96
Akile ODAY <Akile ODAY@DESKTOP-VC6BBKV>
2021-04-13 11:50:08 +0300

    13 Nisan 2021

--- WindowsApplication2/Form1.Designer.vb
+++ WindowsApplication2/Form1.Designer.vb
@@ -22,100 +22,200 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.ekleButton = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.GroupBox1.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.ekleButton)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Location = New System.Drawing.Point(36, 51)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(456, 130)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(72, 31)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(25, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Isim"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(72, 71)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(25, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Yaş"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(167, 24)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 2
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(167, 68)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 3
-        '
-        'ekleButton
-        '
-        Me.ekleButton.Location = New System.Drawing.Point(352, 91)
-        Me.ekleButton.Name = "ekleButton"
-        Me.ekleButton.Size = New System.Drawing.Size(75, 23)
-        Me.ekleButton.TabIndex = 4
-        Me.ekleButton.Text = "Ekle"
-        Me.ekleButton.UseVisualStyleBackColor = True
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(36, 205)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
-        Me.DataGridView1.TabIndex = 5
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(579, 367)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.IDLabel = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.TCRadioButton = New System.Windows.Forms.RadioButton()
+		Me.KKTCRadioButton = New System.Windows.Forms.RadioButton()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.ekleButton = New System.Windows.Forms.Button()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+		Me.silButton = New System.Windows.Forms.Button()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.GroupBox1.SuspendLayout()
+		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.Button1)
+		Me.GroupBox1.Controls.Add(Me.silButton)
+		Me.GroupBox1.Controls.Add(Me.IDLabel)
+		Me.GroupBox1.Controls.Add(Me.Label4)
+		Me.GroupBox1.Controls.Add(Me.TCRadioButton)
+		Me.GroupBox1.Controls.Add(Me.KKTCRadioButton)
+		Me.GroupBox1.Controls.Add(Me.Label3)
+		Me.GroupBox1.Controls.Add(Me.ekleButton)
+		Me.GroupBox1.Controls.Add(Me.TextBox2)
+		Me.GroupBox1.Controls.Add(Me.TextBox1)
+		Me.GroupBox1.Controls.Add(Me.Label2)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Location = New System.Drawing.Point(36, 23)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(657, 219)
+		Me.GroupBox1.TabIndex = 0
+		Me.GroupBox1.TabStop = False
+		'
+		'IDLabel
+		'
+		Me.IDLabel.AutoSize = True
+		Me.IDLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.IDLabel.Location = New System.Drawing.Point(169, 33)
+		Me.IDLabel.Name = "IDLabel"
+		Me.IDLabel.Size = New System.Drawing.Size(14, 18)
+		Me.IDLabel.TabIndex = 10
+		Me.IDLabel.Text = "-"
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label4.Location = New System.Drawing.Point(34, 33)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(24, 18)
+		Me.Label4.TabIndex = 9
+		Me.Label4.Text = "ID"
+		'
+		'TCRadioButton
+		'
+		Me.TCRadioButton.AutoSize = True
+		Me.TCRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.TCRadioButton.Location = New System.Drawing.Point(248, 140)
+		Me.TCRadioButton.Name = "TCRadioButton"
+		Me.TCRadioButton.Size = New System.Drawing.Size(48, 22)
+		Me.TCRadioButton.TabIndex = 8
+		Me.TCRadioButton.TabStop = True
+		Me.TCRadioButton.Text = "TC"
+		Me.TCRadioButton.UseVisualStyleBackColor = True
+		'
+		'KKTCRadioButton
+		'
+		Me.KKTCRadioButton.AutoSize = True
+		Me.KKTCRadioButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.KKTCRadioButton.Location = New System.Drawing.Point(172, 140)
+		Me.KKTCRadioButton.Name = "KKTCRadioButton"
+		Me.KKTCRadioButton.Size = New System.Drawing.Size(70, 22)
+		Me.KKTCRadioButton.TabIndex = 7
+		Me.KKTCRadioButton.TabStop = True
+		Me.KKTCRadioButton.Text = "KKTC"
+		Me.KKTCRadioButton.UseVisualStyleBackColor = True
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label3.Location = New System.Drawing.Point(34, 140)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(52, 18)
+		Me.Label3.TabIndex = 6
+		Me.Label3.Text = "Uyruk"
+		'
+		'ekleButton
+		'
+		Me.ekleButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.ekleButton.Location = New System.Drawing.Point(394, 178)
+		Me.ekleButton.Name = "ekleButton"
+		Me.ekleButton.Size = New System.Drawing.Size(82, 35)
+		Me.ekleButton.TabIndex = 4
+		Me.ekleButton.Text = "Ekle"
+		Me.ekleButton.UseVisualStyleBackColor = True
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.TextBox2.Location = New System.Drawing.Point(172, 101)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(100, 24)
+		Me.TextBox2.TabIndex = 3
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.TextBox1.Location = New System.Drawing.Point(172, 57)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(100, 24)
+		Me.TextBox1.TabIndex = 2
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(34, 91)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(36, 18)
+		Me.Label2.TabIndex = 1
+		Me.Label2.Text = "Yaş"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(34, 57)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(39, 18)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Isim"
+		'
+		'DataGridView1
+		'
+		Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+		Me.DataGridView1.Location = New System.Drawing.Point(38, 294)
+		Me.DataGridView1.Name = "DataGridView1"
+		Me.DataGridView1.Size = New System.Drawing.Size(430, 150)
+		Me.DataGridView1.TabIndex = 5
+		'
+		'silButton
+		'
+		Me.silButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.silButton.Location = New System.Drawing.Point(521, 178)
+		Me.silButton.Name = "silButton"
+		Me.silButton.Size = New System.Drawing.Size(73, 35)
+		Me.silButton.TabIndex = 6
+		Me.silButton.Text = "Sil"
+		Me.silButton.UseVisualStyleBackColor = True
+		'
+		'Button1
+		'
+		Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button1.Location = New System.Drawing.Point(311, 57)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 55)
+		Me.Button1.TabIndex = 11
+		Me.Button1.Text = "Bilgi Getir"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(760, 477)
+		Me.Controls.Add(Me.DataGridView1)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
 
-    End Sub
+	End Sub
 
-    Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents ekleButton As Button
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents DataGridView1 As DataGridView
+	Friend WithEvents IDLabel As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents TCRadioButton As RadioButton
+	Friend WithEvents KKTCRadioButton As RadioButton
+	Friend WithEvents Label3 As Label
+	Friend WithEvents silButton As Button
+	Friend WithEvents Button1 As Button
 End Class
--- WindowsApplication2/Form1.vb
+++ WindowsApplication2/Form1.vb
@@ -3,14 +3,16 @@ Public Class Form1
 	Dim cnn As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Akile ODAY\OneDrive - Eastern Mediterranean University\Desktop\bahar 2019-2020\BTEP210\ders notlari\5-veritabani bilgi girisi\WindowsApplication2(ozluk)\WindowsApplication2\Database1.mdf"";Integrated Security=True")
 	Dim cmd As New SqlCommand
     Dim ds As New DataSet
-    Dim da As New SqlDataAdapter
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+	Dim da As New SqlDataAdapter
+	Dim rdr As SqlDataReader
+	Dim uyruk As String = ""
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If cnn.State = ConnectionState.Closed Then
             cnn.Open()
             cmd.Connection = cnn
             cmd.CommandType = CommandType.Text
-            cmd.CommandText = "select isim, yas from [ozluk]"
-            da.SelectCommand = cmd
+			cmd.CommandText = "select * from ozluk"
+			da.SelectCommand = cmd
             da.Fill(ds)
             DataGridView1.DataSource = ds.Tables(0)
             cnn.Close()
@@ -23,19 +25,89 @@ Public Class Form1
             ds.Clear()
             cmd.Connection = cnn
             cmd.CommandType = CommandType.Text
-            cmd.Parameters.Clear()
-            cmd.CommandText = "insert into [ozluk](isim,yas) values(@isim,@yas)"
-            cmd.Parameters.Add("@isim", SqlDbType.NVarChar).Value = TextBox1.Text
-            cmd.Parameters.Add("@yas", SqlDbType.NChar).Value = TextBox2.Text
-            cmd.ExecuteNonQuery()
-            MessageBox.Show("kayit eklendi")
+			cmd.Parameters.Clear()
+			If KKTCRadioButton.Checked Then
+				uyruk = "KKTC"
+			Else
+				uyruk = "TC"
+			End If
+			cmd.CommandText = "insert into [ozluk](isim,yas,uyruk) values(@isim,@yas,@uyruk)"
+			cmd.Parameters.Add("@isim", SqlDbType.NVarChar).Value = TextBox1.Text
+			cmd.Parameters.Add("@yas", SqlDbType.NChar).Value = TextBox2.Text
+			cmd.Parameters.Add("@uyruk", SqlDbType.NVarChar).Value = uyruk
+			cmd.ExecuteNonQuery()
+			MessageBox.Show("kayit eklendi")
             TextBox1.Clear()
-            TextBox2.Clear()
-            cmd.CommandText = "select isim,yas from [ozluk]"
-            da.SelectCommand = cmd
+			TextBox2.Clear()
+			KKTCRadioButton.Checked = False
+			TCRadioButton.Checked = False
+			cmd.CommandText = "select * from [ozluk]"
+			da.SelectCommand = cmd
             da.Fill(ds)
             DataGridView1.DataSource = ds.Tables(0)
             cnn.Close()
         End If
+    End Sub
+
+	Private Sub silButton_Click(sender As Object, e As EventArgs) Handles silButton.Click
+		If cnn.State = ConnectionState.Closed Then
+			cnn.Open()
+		End If
+		ds.Clear()
+		rdr.Close()
+		cmd.Connection = cnn
+			cmd.CommandType = CommandType.Text
+		cmd.Parameters.Clear()
+
+		If KKTCRadioButton.Checked Then
+				uyruk = "KKTC"
+			Else
+				uyruk = "TC"
+			End If
+			cmd.CommandText = "delete from [ozluk] where isim=@isim"
+			cmd.Parameters.Add("@isim", SqlDbType.NVarChar).Value = TextBox1.Text
+
+			cmd.ExecuteNonQuery()
+
+			MessageBox.Show("kayit silindi")
+			TextBox1.Clear()
+			TextBox2.Clear()
+			KKTCRadioButton.Checked = False
+		TCRadioButton.Checked = False
+		IDLabel.Text = "-"
+		cmd.CommandText = "select * from [ozluk]"
+			da.SelectCommand = cmd
+			da.Fill(ds)
+			DataGridView1.DataSource = ds.Tables(0)
+			cnn.Close()
+
+	End Sub
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		If cnn.State = ConnectionState.Closed Then
+			cnn.Open()
+			cmd.Connection = cnn
+			cmd.CommandType = CommandType.Text
+			cmd.Parameters.Clear()
+			cmd.CommandText = "select * from [ozluk] where isim=@isim"
+			cmd.Parameters.Add("@isim", SqlDbType.NVarChar).Value = TextBox1.Text
+			rdr = cmd.ExecuteReader()
+			If rdr.HasRows Then
+				rdr.Read()
+				TextBox2.Text = rdr("yas")
+				If (rdr("uyruk").ToString.ToUpper = "KKTC") Then
+					KKTCRadioButton.Checked = True
+				ElseIf (rdr("uyruk").ToString.ToUpper = "TC") Then
+					TCRadioButton.Checked = True
+				End If
+				IDLabel.Text = rdr("Id")
+			Else
+				IDLabel.Text = "-"
+				TextBox2.Clear()
+				KKTCRadioButton.Checked = False
+				TCRadioButton.Checked = False
+
+			End If
+		End If
     End Sub
 End Class
